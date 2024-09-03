7e4db8af502ea09cb41365d3a98d3836a1cb8c8a
Shovon0017 <Shimantomostofa@gmail.com>
2024-09-03 23:15:46 +0600

    medico

--- lib/view/screen/language selection/language_selection.dart
+++ lib/view/screen/language selection/language_selection.dart
@@ -1,16 +1,26 @@
 
 import 'package:flutter/material.dart';
+import 'package:medico/common%20widget/common_button.dart';
 
 class LanguageSelection extends StatelessWidget {
   const LanguageSelection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold( backgroundColor:Color(0xffFFFFFF),
       body: Center(
-        child: Column(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Image.asset("images/english select logo.png"),
+            SizedBox(height: 90),
+            CommonButton(buttonColor:Colors.grey ,
+                buttonName: "বাংলা",textColor: Colors.white, onTap: (){
 
+            }),
+            CommonButton(buttonColor:Color(0xff22A6FE) ,
+                buttonName: "English", onTap: (){
+
+            })
           ],
         ),
       ),
