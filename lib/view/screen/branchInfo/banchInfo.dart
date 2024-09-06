6a20320c439ab88e287d1c13407d0796b9b933f9
Shovon0017 <Shimantomostofa@gmail.com>
2024-09-06 23:35:54 +0600

    medico

---
+++ lib/view/screen/branchInfo/banchInfo.dart
@@ -0,0 +1,182 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:medico/common%20widget/common_button.dart';
+import 'package:medico/view/screen/doctorInfo/doctorInfo.dart';
+
+class BranchInfo extends StatelessWidget {
+  const BranchInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold( backgroundColor: Color(0xffFFFFFF),
+      appBar:AppBar(
+        backgroundColor: Color(0xffFFFFFF),
+        title: SizedBox(
+            height: 32,
+            width: 114,
+            child: Image.asset("images/splash logo.png")),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(Icons.notifications_active_outlined),
+          )
+        ],
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              Center(child: Image.asset("images/hospitaldetails.png")),
+              Row(mainAxisAlignment:MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.location_on),
+                    Text("MymenSingh",style: TextStyle(fontSize: 20)),
+                    Icon(Icons.arrow_drop_down),
+                  ]
+              ),
+              Text("Delta Health Care, Mymensingh Ltd",style: TextStyle(fontSize: 23),),
+              SizedBox(height: 150,
+              width: 340,child: Card(
+                  elevation: 3,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("Doctor List",
+                      style:
+                      TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  Container(
+                      width: 190,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Icon(Icons.lan),
+                            Text("All", style: TextStyle(fontSize: 20)),
+                            Icon(Icons.arrow_drop_down),
+                          ]))
+                ],
+              ),
+              Expanded(
+                child: ListView.builder(
+                    itemCount: 8,
+                    physics: PageScrollPhysics(),
+                    scrollDirection: Axis.vertical,
+                    itemBuilder: (context, index) {
+                      return SizedBox(
+                        height: 250,
+                        width: double.infinity,
+                        child: InkWell(onTap: (){
+                          Get.to(()=>DoctorInfo());
+                        },
+                          child: Card(
+                            elevation: 5,
+                            color: Color(0xffFFFFFF),
+                            child: Row(
+                              children: [
+                                Container(
+                                  child: Image.asset("images/doctorPic.png",
+                                      fit: BoxFit.fill),
+                                ),
+                                SizedBox(width: 20),
+                                Expanded(
+                                  child: Container(
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment:
+                                      CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          "Assoc. Prof. Dr. Khandker Parvez Ahmad",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 20),
+                                        ),
+                                        SizedBox(height: 5),
+                                        Row(
+                                          children: [
+                                            Text(
+                                              "Specialties:",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            CommonButton(
+                                                buttonWidth: 110,
+                                                buttonHeight: 25,
+                                                buttonColor: Colors.blue,
+                                                buttonName: "Neurologist",
+                                                onTap: () {})
+                                          ],
+                                        ),
+                                        Text("MBBS, Phd (Neurology) (ITALY), MSc (Endocrinology) (UK)"),
+                                        SizedBox(height: 5),
+                                        RichText(
+                                            text: TextSpan(children: [
+                                              TextSpan(
+                                                  text: "Working:",
+                                                  style: TextStyle(
+                                                      fontWeight: FontWeight.bold,
+                                                      color: Colors.black)),
+                                              TextSpan(
+                                                  text: "Victoria Healthcare",
+                                                  style:
+                                                  TextStyle(color: Colors.black))
+                                            ])),
+                                        SizedBox(height: 5),
+                                        RichText(
+                                            text: TextSpan(children: [
+                                              TextSpan(
+                                                  text: "BMDC Number:",
+                                                  style: TextStyle(
+                                                      fontWeight: FontWeight.bold,
+                                                      color: Colors.black)),
+                                              TextSpan(
+                                                  text: "M37103",
+                                                  style:
+                                                  TextStyle(color: Colors.black))
+                                            ])),
+                                        SizedBox(height: 5),
+                                        RichText(
+                                            text: TextSpan(children: [
+                                              TextSpan(
+                                                  text: "experience:",
+                                                  style: TextStyle(
+                                                      fontWeight: FontWeight.bold,
+                                                      color: Colors.black)),
+                                              TextSpan(
+                                                  text: "17+ Years",
+                                                  style:
+                                                  TextStyle(color: Colors.black))
+                                            ]))
+                                      ],
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/view/screen/hospitalInfo/hospitalInfo.dart
+++ lib/view/screen/hospitalInfo/hospitalInfo.dart
@@ -28,81 +28,146 @@ class HospitalInfo extends StatelessWidget {
       body:Center(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Hospital Info",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Center(child: Image.asset("images/hospitaldetails.png")),
-
-              Row(
-
-                children: [
-                  Text(
-                    "Type:",
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+          child: SingleChildScrollView(
+            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Hospital Info",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Center(child: Image.asset("images/hospitaldetails.png")),
+            
+                Row(
+            
+                  children: [
+                    Text(
+                      "Type:",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
+                    ),
+                    CommonButton(
+                        buttonColor: Colors.blue,
+                        buttonHeight: 25,
+                        buttonWidth: 120,
+                        buttonName: "Specialties",
+                        onTap: () {}),
+            
+                    RichText(
+                        text: TextSpan(children: [
+                          TextSpan(
+                              text: "Experience:",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                  fontSize: 15)),
+                          TextSpan(
+                              text: "8+ Years",
+                              style: TextStyle(fontSize: 15, color: Colors.black))
+                        ]))
+                  ],
+                ),
+                RichText(
+                    text: TextSpan(children: [
+                      TextSpan(
+                          text: "Top Branch:",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 15)),
+                      TextSpan(
+                          text: "4",
+                          style: TextStyle(fontSize: 15, color: Colors.black))
+                    ])),
+                RichText(
+                    text: TextSpan(children: [
+                      TextSpan(
+                          text: "Code Number:",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                              fontSize: 15)),
+                      TextSpan(
+                          text: "M37103",
+                          style: TextStyle(fontSize: 15, color: Colors.black))
+                    ])),
+                Text(
+                  "Chamber & Time:",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Text(
+                  "About",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                Text(
+                  "A good number of physicians have served with reputation. Much emphasis has been given to quality services with the help of adequate number of diploma holding trained nurses. Modern medical business is very competitive. Almost every year new world class machines are invented in order to compete with others. It has around one hundred reputed specialist doctors, medical expertise, medical technology and modern management. Hence, it can take challenge in any sort of health care. The hospital is going to create facilities for research & development (R & D) to develop better strategies for the prevention and treatment of various diseases.",
+                ),
+                Text("Contact 24/7",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                Text("01755598365"),
+                Text("01317330599"),
+                Text("deltacarerampura@gmail.com"),
+                Text("Branch Name",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+                SizedBox(
+                  height: 60,
+                  width: double.infinity,
+                  child: Card(
+                    elevation: 5,
+                    color: Color(0xffFFFFFF),
+                    child: Row(
+                      children: [
+                        Icon(Icons.keyboard_arrow_right_outlined),
+                        Text("Delta Health Care,Mirpur Ltd"),
+                      ],
+                    ),
                   ),
-                  CommonButton(
-                      buttonColor: Colors.blue,
-                      buttonHeight: 25,
-                      buttonWidth: 120,
-                      buttonName: "Specialties",
-                      onTap: () {}),
-
-                  RichText(
-                      text: TextSpan(children: [
-                        TextSpan(
-                            text: "Experience:",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.black,
-                                fontSize: 15)),
-                        TextSpan(
-                            text: "8+ Years",
-                            style: TextStyle(fontSize: 15, color: Colors.black))
-                      ]))
-                ],
-              ),
-              RichText(
-                  text: TextSpan(children: [
-                    TextSpan(
-                        text: "Top Branch:",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                            fontSize: 15)),
-                    TextSpan(
-                        text: "4",
-                        style: TextStyle(fontSize: 15, color: Colors.black))
-                  ])),
-              RichText(
-                  text: TextSpan(children: [
-                    TextSpan(
-                        text: "Code Number:",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                            fontSize: 15)),
-                    TextSpan(
-                        text: "M37103",
-                        style: TextStyle(fontSize: 15, color: Colors.black))
-                  ])),
-              Text(
-                "Chamber & Time:",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              ),
-              Text(
-                "About",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              ),
-              Text(
-                "A good number of physicians have served with reputation. Much emphasis has been given to quality services with the help of adequate number of diploma holding trained nurses. Modern medical business is very competitive. Almost every year new world class machines are invented in order to compete with others. It has around one hundred reputed specialist doctors, medical expertise, medical technology and modern management. Hence, it can take challenge in any sort of health care. The hospital is going to create facilities for research & development (R & D) to develop better strategies for the prevention and treatment of various diseases.",
-              )
-            ],
+                ),
+                SizedBox(
+                  height: 60,
+                  width: double.infinity,
+                  child: Card(
+                    elevation: 5,
+                    color: Color(0xffFFFFFF),
+                    child: Row(
+                      children: [
+                        Icon(Icons.keyboard_arrow_right_outlined),
+                        Text("Delta Health Care,Mymensingh Ltd"),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                  width: double.infinity,
+                  child: Card(
+                    elevation: 5,
+                    color: Color(0xffFFFFFF),
+                    child: Row(
+                      children: [
+                        Icon(Icons.keyboard_arrow_right_outlined),
+                        Text("Delta Health Care,Chittagong Ltd"),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                  width: double.infinity,
+                  child: Card(
+                    elevation: 5,
+                    color: Color(0xffFFFFFF),
+                    child: Row(
+                      children: [
+                        Icon(Icons.keyboard_arrow_right_outlined),
+                        Text("Delta Health Care,Jattrabari Ltd"),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
