8df8a3caf9fda97467252cfc7c0a069f66a0724a
Shovon0017 <Shimantomostofa@gmail.com>
2024-09-07 02:15:45 +0600

    medico

--- lib/view/screen/payLater/payLater.dart
+++ lib/view/screen/payLater/payLater.dart
@@ -8,7 +8,20 @@ class Paylater extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Color(0xffFFFFFF),
+        title: SizedBox(
+            height: 32,
+            width: 114,
+            child: Image.asset("images/splash logo.png")),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(Icons.notifications_active_outlined),
+          )
+        ],
+      ),
     );
   }
 }
